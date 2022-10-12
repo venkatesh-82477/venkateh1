f3cc887091dc71d60b773f9b8c814123facad0a0
venkatesh-82477 <109161764+venkatesh-82477@users.noreply.github.com>
2022-10-12 16:41:58 +0530

    Update JSDAY6.js

--- JSDAY6.js
+++ JSDAY6.js
@@ -88,29 +88,6 @@ console.log(string);
 
 //Write a code to create two objects with 2 properties each, one will be string and other will be an array. Create a function to check if all the elements of the arrays in both the objects are same//
 
-let obj1 = {
-    name: 'Ram',
-    hobbies: ['Cricket', 'Swimming']
-};
-
-let obj2 = {
-    name: 'Ram',
-    hobbies: ['Football', 'Swimming']
-};
-const haveSameData = function(obj1, obj2) {
-    const obj1Length = Object.keys(obj1).length;
-    const obj2Length = Object.keys(obj2).length;
-
-    if(obj1Length === obj2Length) {
-        return Object.keys(obj1).every(
-            key => obj2.hasOwnProperty(key)
-               && obj2[key] === obj1[key]);
-    }
-    return false;
-}
-console.log(haveSameData(obj1, obj2));
-
-
 let data1 ={
     name :"venkatesh",
     subjects :["maths","AEC","CONTROL systems"]
